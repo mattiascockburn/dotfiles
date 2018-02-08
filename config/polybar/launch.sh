c138b8375aff8be59e65917b924cc8936a42c0c6
Mattias Giese <giese@b1-systems.de>
2018-02-08 23:29:30 +1300

    polybar: add a hacky way to determine wifi adapter in launch script

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -6,6 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+WIFI_ADAPTER=$(nmcli device status |grep -m 1 wifi |awk '{ print $1; };')
+
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload example &
