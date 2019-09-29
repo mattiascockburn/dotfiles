4f756562d43b3fe3b8ad07d90c814132c38ce0c9
Mattias Giese <mattiasgiese@posteo.net>
2019-09-29 14:37:22 -0500

    polybar/amarula: hide bar by default (Super+(Shift+), hotkey in i3

---
+++ host-amarula/polybar/launch.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+WIFI_ADAPTER=$(nmcli device status |grep -m 1 wifi |awk '{ print $1; };')
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
+
+sleep 1
+polybar-msg cmd hide
+
+echo "Bars launched..."
