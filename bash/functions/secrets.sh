aeb555435b952aec7ec949a055338b69b679da4b
Mattias Giese <mattiasgiese@posteo.net>
2019-10-30 09:34:15 -0500

    bash: adjust otp function
    Add B1 gitlab account reference

--- bash/functions/secrets.sh
+++ bash/functions/secrets.sh
@@ -2,5 +2,6 @@ otp() {
   declare -a otp
   otp[posteo]='internet/dienstleister/posteo'
   otp[giz]='giz/giz.de'
+  otp[b1git]='b1/gitlab'
   oathtool -b --totp $(pass show ${otp[$1]} | grep OTP | cut -d' ' -f2)
 }
