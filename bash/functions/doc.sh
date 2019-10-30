d9a772035b6a5cf3a8611912ecf043779d936cce
Mattias Giese <mattiasgiese@posteo.net>
2019-10-30 09:32:07 -0500

    bash: add docstat function
    Janky function which quickly sets or gets the current doc:status
    property of a file in a SVN repository

--- bash/functions/doc.sh
+++ bash/functions/doc.sh
@@ -8,3 +8,22 @@ watchbuild() {
   echo "$1" | entr bash -c "doc-build \"$1\""
 }
 alias wb=watchbuild
+
+# set/get doc:status
+docstat() {
+  [[ "$#" -lt 1 ]] && {
+    exit Fail
+    return
+  }
+  mod=$1
+  shift
+  case "$mod" in
+    get)
+      svn propget doc:status $@
+      ;;
+    set)
+      prop=$1
+      shift
+      svn propset doc:status $prop $@
+  esac
+}
