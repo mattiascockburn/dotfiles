ce6fa81e0edac54b30ca445df369152d91664a7d
Mattias Giese <mattiasgiese@posteo.net>
2020-07-16 12:40:45 -0500

    zsh: better escaping/var expansion in doc-build function

--- config/zsh/functions/doc-build.sh
+++ config/zsh/functions/doc-build.sh
@@ -5,10 +5,10 @@ doc-build() {
   if [ -n "$TEXMF" ]; then
     DOCKER_OPTS="${DOCKER_OPTS} -v${TEXMF}:/${BASEDIR}/texmf:ro"
   fi
-  CMD="cd ~/svn; make $@"
+  CMD="cd ${BASEDIR}; make $*"
   # puppet docs "automagic"
   if [ -x ./inputdoc ]; then
-    [[ -z "$1" ]] && CMD='cd ${BASEDIR}; ./inputdoc'
+    [[ -z "$1" ]] && CMD="cd ${BASEDIR}; ./inputdoc"
   fi
   if [ "$1" = 'shell' ]; then
     CMD='/bin/bash'
@@ -25,6 +25,6 @@ doc-build() {
     fi
     CMD="cd ${BASEDIR}; $2"
   fi
-  podman run -t -i --net=none --userns keep-id --rm $DOCKER_OPTS -v $(pwd):${BASEDIR}:z $IMAGE bash -c "$CMD"
+  podman run -t -i --net=none --userns keep-id --rm $DOCKER_OPTS -v "$(pwd):${BASEDIR}:z" "$IMAGE" bash -c "$CMD"
 }
 
