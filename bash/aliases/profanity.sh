6b8885df3a6ebcc450beb33a5e5a975841b9bf88
Mattias Giese <mattiasgiese@posteo.net>
2020-09-21 10:48:38 -0500

    tmux/bash: fix alias for profanity multi account setup

--- bash/aliases/profanity.sh
+++ bash/aliases/profanity.sh
@@ -1,1 +1,1 @@
-alias jabber='tmux new-session "tmux source-file ~/.config/tmux/sessions/profanity.conf"'
+alias jabber='tmux new-session -A -s jabber "tmux source-file ~/.config/tmux/sessions/profanity.conf"'
