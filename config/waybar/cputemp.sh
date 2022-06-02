4d80d05a583e506270a1fa16106524f74578a067
Mattias Giese <mattiasgiese@posteo.net>
2022-06-02 22:14:52 +0200

    waybar: enable cputemp info for chilcano

--- config/waybar/cputemp.sh
+++ config/waybar/cputemp.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sensors -j acpitz-acpi-0 | jq '.["acpitz-acpi-0"]["temp1"]["temp1_input"]'
+h=$(hostnamectl hostname)
+[[ "$h" = 'wasabi' ]] && sensors -j acpitz-acpi-0 | jq '.["acpitz-acpi-0"]["temp1"]["temp1_input"]'
+[[ "$h" = 'chilcano' ]] && sensors -j thinkpad-isa-0000 2>/dev/null | jq '.["thinkpad-isa-0000"]["CPU"]["temp1_input"]'
