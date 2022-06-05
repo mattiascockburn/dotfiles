cc80431fdf47b5ebb682e9956e9f56da5a905c65
Mattias Giese <mattiasgiese@posteo.net>
2022-06-05 11:26:24 +0200

    waybar: make sure cputemp script exits successfully

--- config/waybar/cputemp.sh
+++ config/waybar/cputemp.sh
@@ -2,3 +2,4 @@
 h=$(hostnamectl hostname)
 [[ "$h" = 'wasabi' ]] && sensors -j acpitz-acpi-0 | jq '.["acpitz-acpi-0"]["temp1"]["temp1_input"]'
 [[ "$h" = 'chilcano' ]] && sensors -j thinkpad-isa-0000 2>/dev/null | jq '.["thinkpad-isa-0000"]["CPU"]["temp1_input"]'
+true
