0c5d702115160d37e345d24cac7d0fa24bc13b36
Mattias Cockburn <mattias.cockburn@posteo.net>
2023-01-15 16:19:44 +0100

    nvim: add better packer bootstrap, stolen from nvim-kickstart

--- config/nvim/lua/user/plugins.lua
+++ config/nvim/lua/user/plugins.lua
@@ -1,4 +1,10 @@
-local fn = vim.fn
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local is_bootstrap = false
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  is_bootstrap = true
+  vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
+  vim.cmd [[packadd packer.nvim]]
+end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
 vim.cmd([[
@@ -145,4 +151,9 @@ return packer.startup(function(use)
 
   -- Movement
   use("ggandor/leap.nvim")
+
+  if is_bootstrap then
+    require('packer').sync()
+  end
+
 end)
