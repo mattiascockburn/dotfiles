bd89df20ee0b7173bb02fd3a6ecc4bcf1d3a1915
Mattias Cockburn <mattias.cockburn@posteo.net>
2023-09-27 11:26:49 +0200

    nvim: refine focus configuration

--- config/nvim/lua/user/focus.lua
+++ config/nvim/lua/user/focus.lua
@@ -7,3 +7,34 @@ focusmap('h')
 focusmap('j')
 focusmap('k')
 focusmap('l')
+
+local ignore_filetypes = { 'NvimTree_1' }
+local ignore_buftypes = { 'nofile', 'prompt', 'popup' }
+
+local augroup =
+    vim.api.nvim_create_augroup('FocusDisable', { clear = true })
+
+vim.api.nvim_create_autocmd('WinEnter', {
+  group = augroup,
+  callback = function(_)
+    if vim.tbl_contains(ignore_buftypes, vim.bo.buftype)
+    then
+      vim.w.focus_disable = true
+    else
+      vim.w.focus_disable = false
+    end
+  end,
+  desc = 'Disable focus autoresize for BufType',
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup,
+  callback = function(_)
+    if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+      vim.w.focus_disable = true
+    else
+      vim.w.focus_disable = false
+    end
+  end,
+  desc = 'Disable focus autoresize for FileType',
+})
